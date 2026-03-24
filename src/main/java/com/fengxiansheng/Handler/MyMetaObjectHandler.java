f314410fc636a0d28924bba43d6e821a3d2fad37
laoyase <laotase@126.com>
2026-03-24 10:51:44 +0800

    mybatis-plus实现数据的自动填充

---
+++ src/main/java/com/fengxiansheng/Handler/MyMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.fengxiansheng.Handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+//        this.strictInsertFill(metaObject,"createUserId",Long.class,123456L);
+        this.strictInsertFill(metaObject,"createTime", LocalDateTime.class,LocalDateTime.now());
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+//        this.strictInsertFill(metaObject,"updateUserId", Long.class,123456L);
+        this.strictUpdateFill(metaObject,"updateTime",LocalDateTime.class,LocalDateTime.now());
+
+    }
+}
